7cae013dc07bc6942f6cbd8668dc5478b090fd50
Abdullah Tareq <abdullah.mohammad02@cba.ku.edu.kw>
2021-12-12 12:01:36 +0300

    blog

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+
+    STATS = ( 
+        (0, "Draft")
+        (1, "Published")
+    )
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField(null=True, blank=True)
     craeted_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.IntegerField()
+    status = models.IntegerField(choices=STATS, default=0)
